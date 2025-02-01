c232ae106de0829f4af876df65d00cc57b1a8d76
Omar Palacios <omarpal88@gmail.com>
2025-01-31 23:58:55 -0500

    :package: update webpack config file, install babel and raw-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,28 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
     filename: 'main.js',
   },
+  resolve: {
+    extensions: ['.js', '.scss', '.liquid'],
+  },
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.liquid$/,
+        use: 'raw-loader',
       },
     ],
   },
